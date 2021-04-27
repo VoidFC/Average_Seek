cd9162b46e5a59018a7840fc9464a58f3c701108
FC <83190450+VoidFC@users.noreply.github.com>
2021-04-26 20:07:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AverageSeek_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declares variables
+            double num01;
+            double num02;
+            double num03;
+            double sum;
+            double average;
+
+            double result;
+
+            //gets user input for variables
+            Console.Write("Please pick a number: " );
+            num01 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please pick a second number: ");
+            num02 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please pick a third number: ");
+            num03 = Convert.ToDouble(Console.ReadLine());
+
+            //finds average of the three, and outputs it
+            sum = num01 + num02 + num03;
+            average = sum / 3;
+
+            Console.Write("The sum of those numbers is " + sum + "\nThe average of those numbers is: " + average);
+
+            //waits before closing
+            Console.ReadKey();
+        }
+    }
+}
